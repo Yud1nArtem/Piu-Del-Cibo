672475f46fb6d064c9e0e95cddff004fc1806c84
Yud1nArtem <yudin20011610@gmail.com>
2021-02-23 19:39:44 +0300

    Add concat to style.min.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 const { src, dest } = require('gulp');
 
 const scss = require('gulp-sass');
+const concat = require('gulp-concat');
 
 
 function styles() {
     return src('app/scss/style.scss')
-        .pipe(scss())
+        .pipe(scss({outputStyle: 'compressed'}))
+        .pipe(concat('style.min.css'))
         .pipe(dest('app/css'))
 }
 
