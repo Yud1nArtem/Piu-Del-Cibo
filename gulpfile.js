bd04e7bbed54a9b8c66828942530a67305e8724b
Yud1nArtem <yudin20011610@gmail.com>
2021-02-23 21:39:16 +0300

    Add build

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 const { src, dest, watch, parallel } = require('gulp');
 
-const scss        = require('gulp-sass');
-const concat      = require('gulp-concat');
-const browserSync = require('browser-sync').create();
-const uglify      = require('gulp-uglify-es').default;
+const scss         = require('gulp-sass');
+const concat       = require('gulp-concat');
+const browserSync  = require('browser-sync').create();
+const uglify       = require('gulp-uglify-es').default;
+const autoprefixer = require('gulp-autoprefixer');
 
 function browsersync() {
     browserSync.init({
@@ -28,8 +29,23 @@ function styles() {
     return src('app/scss/style.scss')
         .pipe(scss({outputStyle: 'compressed'}))
         .pipe(concat('style.min.css'))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 10 version'],
+            grid: true
+        }))
         .pipe(dest('app/css'))
         .pipe(browserSync.stream())
+}
+
+
+function build() {
+    return src([
+        'app/css/style.min.css',
+        'app/fonts/**/*',
+        'app/js/main.min.js',
+        'app/*.html'
+    ], {base: 'app'})
+    .pipe(dest('dist'))
 }
 
 function watching() {
@@ -42,5 +58,6 @@ exports.styles = styles;
 exports.watching = watching;
 exports.browsersync = browsersync;
 exports.scripts = scripts;
+exports.build = build;
 
 exports.default = parallel(scripts, browsersync, watching);
