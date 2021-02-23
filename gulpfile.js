50e213548bfe50d8c1a2791042ef0c3ae24c2ef6
Yud1nArtem <yudin20011610@gmail.com>
2021-02-23 19:50:54 +0300

    Add watching for *.scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { src, dest } = require('gulp');
+const { src, dest, watch } = require('gulp');
 
 const scss = require('gulp-sass');
 const concat = require('gulp-concat');
@@ -11,4 +11,9 @@ function styles() {
         .pipe(dest('app/css'))
 }
 
+function watching() {
+    watch(['app/scss/**/*.scss'], styles)
+} 
+
 exports.styles = styles;
+exports.watching = watching;
