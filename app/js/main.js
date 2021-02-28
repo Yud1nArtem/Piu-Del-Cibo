40bea75f987d7751dad57b80aa1699f57cb4174c
Yud1nArtem <yudin20011610@gmail.com>
2021-02-28 21:05:09 +0300

    Add section info in html and css files

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,13 @@
 $(function () {
     $('.slider').slick({
         dots: true,
+        speed: 1200,
+        infinite: false,
+        autoplay: true,
+        autoplaySpeed: 10000,
+        pauseOnFocus: true,
+        pauseOnHover: true,
+        pauseOnDotsHover: false,
+        draggable: false,
     });
 });
