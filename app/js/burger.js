98dd09a68627c91b390d70d58434d222959e9e3e
Yud1nArtem <yudin20011610@gmail.com>
2021-03-07 19:11:57 +0300

    Do index.html

--- app/js/burger.js
+++ app/js/burger.js
@@ -9,8 +9,17 @@ $(function () {
 });
 
 $(function () {
-    $('.album__burger_menu').on('click', function () {
+    $('.albums__burger_menu').on('click', function () {
         $('.albums__header__menu-list').toggleClass('active');
+        $('.albums__burger_menu').toggleClass('active');
+    });
+});
+
+$(function () {
+    $('.album__burger_menu').on('click', function () {
+        $('body').toggleClass('active');
+        $('.header__top_inner').toggleClass('active');
+        $('.header_menu').toggleClass('active');
         $('.album__burger_menu').toggleClass('active');
     });
 });
