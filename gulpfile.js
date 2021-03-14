da500516a1ec1b50f59459e023b9ecbfab39d2f6
Yud1nArtem <yudin20011610@gmail.com>
2021-03-14 18:30:08 +0300

    create pizza page and do product css

--- gulpfile.js
+++ gulpfile.js
@@ -63,6 +63,7 @@ function styles() {
             'app/scss/albumFirst.scss',
             'app/scss/discounts.scss',
             'app/scss/delivery.scss',
+            'app/scss/product.scss',
             'app/scss/variables.scss'
         ])
         .pipe(scss({outputStyle: 'compressed'}))
